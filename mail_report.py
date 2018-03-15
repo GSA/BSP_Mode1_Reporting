7bcc9abf6de711fca43038b3cd3e20fe8b6a2ed2
briankfitzwater <brian.fitzwater@gsa.gov>
2018-03-15 08:43:32 -0400

    Fixes for pylint (Closes #11)

--- mail_report.py
+++ mail_report.py
@@ -1,16 +1,24 @@
+"""
+Lambda function for creating emailing daily AMI Report
+
+Sends a CSV report from an S3 bucket as an attachment to a distribution list
+specified in the `recipients` environment variable.
+"""
 import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import json
 import boto3
 from botocore.exceptions import ClientError
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
 
 # Constants
 KEY_ID = os.environ['ami_report_key_id']
 
 def lambda_handler(event, context):
+    """Handler function for Lambda function to mail AMI Report"""
+    del context # Unused
     ses = boto3.client('ses')
-    s3 = boto3.resource('s3')
+    s3_res = boto3.resource('s3')
     s3_key = event['Records'][0]['s3']['object']['key']
     s3_bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = "/tmp/" + s3_key
@@ -18,12 +26,11 @@ def lambda_handler(event, context):
     msg = MIMEMultipart()
     msg['From'] = os.environ['sender']
     msg['To'] = to_emails
-    AWS_REGION = "us-east-1"
     msg['Subject'] = "Amazon AMI Report"
-    CHARSET = "UTF-8"
+    char_set = "UTF-8"
 
     # The HTML body of the email.
-    BODY_HTML = """<html>
+    body_html = """<html>
     <head></head>
     <body>
       <h1>AWS Machine Image (AMI) Report</h1>
@@ -35,11 +42,11 @@ def lambda_handler(event, context):
     </body>
     </html>
                 """
-    part = MIMEText(BODY_HTML, 'html', CHARSET)
+    part = MIMEText(body_html, 'html', char_set)
     msg.attach(part)
 
-    s3.Bucket(s3_bucket).download_file(s3_key, file_name)
-    part = MIMEText(open(file_name, "rb").read(), 'csv', CHARSET)
+    s3_res.Bucket(s3_bucket).download_file(s3_key, file_name)
+    part = MIMEText(open(file_name, "rb").read(), 'csv', char_set)
     part.add_header('Content-Disposition', 'attachment', filename=s3_key)
     msg.attach(part)
 
@@ -48,17 +55,17 @@ def lambda_handler(event, context):
         #Provide the contents of the email.
         ses.send_raw_email(
             RawMessage={
-                 'Data': msg.as_string(),
+                'Data': msg.as_string(),
             },
             Source=msg['From'],
             Destinations=to_emails.split(",")
         )
     # Display an error if something goes wrong.
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+    except ClientError as err:
+        print err.response['Error']['Message']
     else:
-        print("Email sent!")
+        print "Email sent!"
 
 if __name__ == "__main__":
-    json_content = json.loads(open('event.json', 'r').read())
-    lambda_handler(json_content, None)
+    JSON_CONTENT = json.loads(open('event.json', 'r').read())
+    lambda_handler(JSON_CONTENT, None)
