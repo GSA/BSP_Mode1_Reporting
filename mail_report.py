c26975120870170ca7593074e508a90991ce189c
briankfitzwater <brian.fitzwater@gsa.gov>
2018-10-10 17:14:08 -0400

    WIP

--- mail_report.py
+++ mail_report.py
@@ -5,6 +5,7 @@ Sends a CSV report from an S3 bucket as an attachment to a distribution list
 specified in the `recipients` environment variable.
 """
 import os
+import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import json
@@ -13,6 +14,23 @@ from botocore.exceptions import ClientError
 
 # Constants
 KEY_ID = os.environ['ami_report_key_id']
+TODAY = datetime.datetime.today().strftime('%Y-%m-%d')
+
+def get_header(key):
+    """Returns eMail header based on file name"""
+    if key.startswith('ami_report_'):
+        return 'AWS Machine Image (AMI) Report'
+    if key.startswith('snapshot_report_'):
+        return 'AWS Snapshot Report'
+    return 'Report from Lambda Function'
+
+def get_subject(key):
+    """Returns eMail subject based on file name"""
+    if key.startswith('ami_report_'):
+        return "BSP Mode1 AMI Report - " + TODAY
+    if key.startswith('snapshot_report_'):
+        return "BSP Mode1 Snapshot Report - " + TODAY
+    return "BSP Mode1 Report - " + TODAY
 
 def lambda_handler(event, context):
     """Handler function for Lambda function to mail AMI Report"""
@@ -23,17 +41,18 @@ def lambda_handler(event, context):
     s3_bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = "/tmp/" + s3_key
     to_emails = os.environ['recipients']
+    header = get_header(s3_key)
     msg = MIMEMultipart()
     msg['From'] = os.environ['sender']
     msg['To'] = to_emails
-    msg['Subject'] = "Amazon AMI Report"
+    msg['Subject'] = get_subject(s3_key)
     char_set = "UTF-8"
 
     # The HTML body of the email.
     body_html = """<html>
     <head></head>
     <body>
-      <h1>AWS Machine Image (AMI) Report</h1>
+      <h1>{0}</h1>
       <p>Report attached as csv file.</p>
       <p>This email was sent with
         <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
@@ -41,7 +60,7 @@ def lambda_handler(event, context):
           AWS SDK for Python (Boto)</a>.</p>
     </body>
     </html>
-                """
+                """.format(header)
     part = MIMEText(body_html, 'html', char_set)
     msg.attach(part)
 
